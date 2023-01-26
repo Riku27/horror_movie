2103aff8289e8d45a1f349e467bc51f62c929812
EC2 Default User <ec2-user@ip-172-31-14-220.ap-northeast-1.compute.internal>
2023-01-26 11:42:17 +0000

    fix

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,7 +5,6 @@ class SearchesController < ApplicationController
    def index
     @range = params[:range]
     @movies = Movie.looks(params[:search], params[:word]).page(params[:page]).per(8)
-    @movie = params[:tag_id].present? ? Tag.find(params[:tag_id]).movies: Movie.all
    end
    
 end
